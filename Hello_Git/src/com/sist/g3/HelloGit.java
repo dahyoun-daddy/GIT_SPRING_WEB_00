d298a7deca01f2cfcdee9f40c6277cd2b36a7c1a
herbhand <herbhand1@gmail.com>
2017-09-14 19:05:43 +0900

    9090909009

--- Hello_Git/src/com/sist/g3/HelloGit.java
+++ Hello_Git/src/com/sist/g3/HelloGit.java
@@ -5,7 +5,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   //james 추가
-<<<<<<< HEAD
+
 		System.out.println("0000000000000");
 		//9999999999999999999999999999999999
   //james 추가
@@ -17,7 +17,7 @@ public class HelloGit {
 		//9999999999999999999999999999999999
 		//9999999999999999999999999999999999
 		//9999999999999999999999999999999999
-=======
+
 		System.out.println("0000000000000");
 		
 		
@@ -29,7 +29,6 @@ public class HelloGit {
 		System.out.println("11111111111111");
 		System.out.println("0000000000000");
 		System.out.println("0000000000000");		
->>>>>>> branch 'master' of https://github.com/dahyoun-daddy/GIT_SPRING_WEB_00.git
 	}
 
 }
