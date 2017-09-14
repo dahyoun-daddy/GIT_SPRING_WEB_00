7445e5d06c73276f9878a60825eab8ac523954b7
herbhand <herbhand1@gmail.com>
2017-09-14 18:49:41 +0900

    3

--- Hello_Git/src/com/sist/g1/HelloGit6.java
+++ Hello_Git/src/com/sist/g1/HelloGit6.java
@@ -4,6 +4,7 @@ public class HelloGit6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("*************************");
 		System.out.println("hihi");
 		System.out.println("*************************");
 		System.out.println("2*************************"); 
