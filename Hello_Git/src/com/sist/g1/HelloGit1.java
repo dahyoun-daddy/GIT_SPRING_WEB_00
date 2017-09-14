9240fc0f13e819aff4df0216fd0640db46824c89
herbhand <herbhand1@gmail.com>
2017-09-14 18:38:41 +0900

    rrrrr

--- Hello_Git/src/com/sist/g1/HelloGit1.java
+++ Hello_Git/src/com/sist/g1/HelloGit1.java
@@ -4,9 +4,14 @@ public class HelloGit1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//*******************************************
+
+		//-------------------------------
 		System.out.println("hihi");
 		System.out.println("2hihi");
+		System.out.println("j2hihi");
+		System.out.println("j3hihi");
+		//-------------------------------
+
 	}
 
 }
